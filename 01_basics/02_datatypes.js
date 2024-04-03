304bada6f61518abea4ea35622eb01211a51af0a
sanidhyayadav01 <165474475+sanidhyayadav01@users.noreply.github.com>
2024-04-03 15:43:56 +0000

    Data_types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+//treat all the JS code as newer version
+
+//alert(3+3)
+//we are using node not browser
+
+console.log(3+3)
+//code readability should be high
+console.log("Sanidhya Yadav")
+
+let number = null;
+let age = undefined;
+//primitive datatypes:-
+//number = 1234;
+//string="yadav";
+//bigint
+//null;
+//undefined;
+
+console.log(typeof number);
+console.log(typeof age);
