29e795bcdbecc1b65a9efcc0f5694ea44b3b269f
sanidhyayadav01 <165474475+sanidhyayadav01@users.noreply.github.com>
2024-04-04 15:20:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_DatatypeConversion.js
@@ -0,0 +1,34 @@
+/*let score="33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(typeof score)
+console.log(score)
+console.log(valueInNumber)
+*/
+/* 
+"33" =>33
+"33abc"=>NaN
+true=>1 , false=>0
+*/
+let IsLoggedIn = 1
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+
+//console.log(booleanIsLoggedIn)
+
+/*
+1=>true , 0=>false
+"sanidhya"=>true
+" "=>false
+1 ka type conversion boolean me true h and 0 ka false
+empty string ka false h and non-empty string ka true
+*/
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+
+console.log(stringnumber)
+console.log(typeof stringnumber)
