83b480e90d7ce4e9948de427782a11a584af6263
Sanidhya Yadav <165474475+sanidhyayadav01@users.noreply.github.com>
2024-04-06 12:10:20 +0000

    conversion and comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+//console.log(2>1)
+//console.log(2<1)
+//console.log(2>=1)
+//console.log(2==1)
+
+//console.log("2">1)
+//console.log("02">1)
+
+//console.log(null>0)
+//console.log(null<0)
+//console.log(null>=0)
+//console.log(null==0)
+
+// (===)->the tripple equal to operater checks for the same value as well checks for the same datatype also i.e it strictly checks the condition
+
+console.log("2"==="2")
