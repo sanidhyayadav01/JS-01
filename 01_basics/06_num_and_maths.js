14ff13ffc420727659b27142cba028d831f1be2d
Sanidhya Yadav <165474475+sanidhyayadav01@users.noreply.github.com>
2024-04-10 19:17:44 +0000

    numbers and maths

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,12 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(typeof balance)
+
+const otherNumber = 123.8966
+console.log(otherNumber.toFixed(3))
+console.log(otherNumber.toPrecision(2))
