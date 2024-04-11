01a4b1164c35c93d9ee5deadbef402067080e474
Sanidhya Yadav <165474475+sanidhyayadav01@users.noreply.github.com>
2024-04-11 17:09:12 +0000

    num and math

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -1,12 +1,30 @@
 const score = 400
-console.log(score)
+//console.log(score)
 
 const balance = new Number(100)
-console.log(balance)
+//console.log(balance)
 
-console.log(balance.toString().length)
-console.log(typeof balance)
+//console.log(balance.toString().length)
+//console.log(typeof balance)
 
-const otherNumber = 123.8966
-console.log(otherNumber.toFixed(3))
-console.log(otherNumber.toPrecision(2))
+//const otherNumber = 123.8966
+//console.log(otherNumber.toFixed(3))
+//console.log(otherNumber.toPrecision(2))
+
+//+++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+
+// console.log(Math.floor(7.898))
+// console.log(Math.round(4.5678))
+// console.log(Math.ceil(7.000000001))
+// console.log(Math.pow(2,10))
+// console.log(Math.min(1,2,3,4,5))
+// console.log(Math.max(1,2,3,4,5))
+
+
+// console.log(Math.random())
+// console.log(Math.random()*10)
+// console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min + 1) + min))
